8ea25463bc2c322b4d7947b0f56fed3282acf661
Amirhossein Ghasemi <a.ghasemi@iranian.cards>
2021-05-10 12:37:25 +0430

    Remove all object of database

---
+++ Database and configuration/RemoveAllObjectInDatabase.sql
@@ -0,0 +1,45 @@
+declare @n char(1)
+set @n = char(10)
+
+declare @stmt nvarchar(max)
+
+-- procedures
+select @stmt = isnull( @stmt + @n, '' ) +
+    'drop procedure [' + schema_name(schema_id) + '].[' + name + ']'
+from sys.procedures
+
+
+-- check constraints
+select @stmt = isnull( @stmt + @n, '' ) +
+'alter table [' + schema_name(schema_id) + '].[' + object_name( parent_object_id ) + ']    drop constraint [' + name + ']'
+from sys.check_constraints
+
+-- functions
+select @stmt = isnull( @stmt + @n, '' ) +
+    'drop function [' + schema_name(schema_id) + '].[' + name + ']'
+from sys.objects
+where type in ( 'FN', 'IF', 'TF' )
+
+-- views
+select @stmt = isnull( @stmt + @n, '' ) +
+    'drop view [' + schema_name(schema_id) + '].[' + name + ']'
+from sys.views
+
+-- foreign keys
+select @stmt = isnull( @stmt + @n, '' ) +
+    'alter table [' + schema_name(schema_id) + '].[' + object_name( parent_object_id ) + '] drop constraint [' + name + ']'
+from sys.foreign_keys
+
+-- tables
+select @stmt = isnull( @stmt + @n, '' ) +
+    'drop table [' + schema_name(schema_id) + '].[' + name + ']'
+from sys.tables
+
+-- user defined types
+select @stmt = isnull( @stmt + @n, '' ) +
+    'drop type [' + schema_name(schema_id) + '].[' + name + ']'
+from sys.types
+where is_user_defined = 1
+
+
+exec sp_executesql @stmt
