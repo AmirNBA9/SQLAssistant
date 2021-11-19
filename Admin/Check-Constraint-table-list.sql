b7e1980c669890ad49c29996f4ed30929a642648
AmirNBA9 <Amir.h.ghasemi@hotmail.com>
2021-11-19 14:02:33 +0330

    Change folder of encryption method

--- Admin/Check-Constraint-table-list.sql
+++ Admin/Check-Constraint-table-list.sql
@@ -12,20 +12,17 @@ o ‘Disabled’ برای Constraintهای غیرفعال
  Constraint_Name: نام Constraint
 */
 --------------------------------------------------------------------
-select schema_name(t.schema_id) + '.' + t.[name] as [table],
-    col.column_id,
-    col.[name] as column_name,
-    con.[definition],
-    case when con.is_disabled = 0 
-        then 'Active' 
-        else 'Disabled' 
-        end as [status],
-    con.[name] as constraint_name
-from sys.check_constraints con
-    left outer join sys.objects t
-        on con.parent_object_id = t.object_id
-    left outer join sys.all_columns col
-        on con.parent_column_id = col.column_id
-        and con.parent_object_id = col.object_id
-order by schema_name(t.schema_id) + '.' + t.[name], 
-    col.column_id
+SELECT
+	SCHEMA_NAME(t.schema_id) + '.' + t.[Name] AS [table]
+   ,col.column_id
+   ,col.[Name] AS column_name
+   ,con.[definition]
+   ,CASE
+		WHEN con.is_disabled = 0 THEN 'Active'
+		ELSE 'Disabled'
+	END AS [status]
+   ,con.[Name] AS constraint_name
+FROM sys.check_constraints con
+	LEFT OUTER JOIN sys.objects t ON con.parent_object_id = t.object_id
+	LEFT OUTER JOIN sys.all_columns col ON con.parent_column_id = col.column_id AND con.parent_object_id = col.object_id
+ORDER BY SCHEMA_NAME(t.schema_id) + '.' + t.[Name], col.column_id
--- Admin/Constraint-Database-List.sql
+++ Admin/Constraint-Database-List.sql
@@ -23,75 +23,82 @@ Constraint_Name: نام Constraint یا ایندکس
 – Default constraint – نام ستون و مقدار/عبارت تعریف شده برای constrain
 */
 --------------------------------------------------------------------
-    select table_view,
-    object_type,
-    constraint_type,
-    constraint_name,
-    details
-    from (
-    select schema_name(t.schema_id) + '.' + t.[name] as table_view,
-    case when t.[type] = 'U' then 'Table'
-    when t.[type] = 'V' then 'View'
-    end as [object_type],
-    case when c.[type] = 'PK' then 'Primary key'
-    when c.[type] = 'UQ' then 'Unique constraint'
-    when i.[type] = 1 then 'Unique clustered index'
-    when i.type = 2 then 'Unique index'
-    end as constraint_type,
-    isnull(c.[name], i.[name]) as constraint_name,
-    substring(column_names, 1, len(column_names)-1) as [details]
-    from sys.objects t
-    left outer join sys.indexes i
-    on t.object_id = i.object_id
-    left outer join sys.key_constraints c
-    on i.object_id = c.parent_object_id
-    and i.index_id = c.unique_index_id
-    cross apply (select col.[name] + ', '
-    from sys.index_columns ic
-    inner join sys.columns col
-    on ic.object_id = col.object_id
-    and ic.column_id = col.column_id
-    where ic.object_id = t.object_id
-    and ic.index_id = i.index_id
-    order by col.column_id
-    for xml path ('') ) D (column_names)
-    where is_unique = 1
-    and t.is_ms_shipped <> 1
-    union all
-    select schema_name(fk_tab.schema_id) + '.' + fk_tab.name as foreign_table,
-    'Table',
-    'Foreign key',
-    fk.name as fk_constraint_name,
-    schema_name(pk_tab.schema_id) + '.' + pk_tab.name
-    from sys.foreign_keys fk
-    inner join sys.tables fk_tab
-    on fk_tab.object_id = fk.parent_object_id
-    inner join sys.tables pk_tab
-    on pk_tab.object_id = fk.referenced_object_id
-    inner join sys.foreign_key_columns fk_cols
-    on fk_cols.constraint_object_id = fk.object_id
-    union all
-    select schema_name(t.schema_id) + '.' + t.[name],
-    'Table',
-    'Check constraint',
-    con.[name] as constraint_name,
-    con.[definition]
-    from sys.check_constraints con
-    left outer join sys.objects t
-    on con.parent_object_id = t.object_id
-    left outer join sys.all_columns col
-    on con.parent_column_id = col.column_id
-    and con.parent_object_id = col.object_id
-    union all
-    select schema_name(t.schema_id) + '.' + t.[name],
-    'Table',
-    'Default constraint',
-    con.[name],
-    col.[name] + ' = ' + con.[definition]
-    from sys.default_constraints con
-    left outer join sys.objects t
-    on con.parent_object_id = t.object_id
-    left outer join sys.all_columns col
-    on con.parent_column_id = col.column_id
-    and con.parent_object_id = col.object_id) a
-    order by table_view, constraint_type, constraint_name
+SELECT
+	table_view
+   ,object_type
+   ,constraint_type
+   ,constraint_name
+   ,details
+FROM (SELECT
+		SCHEMA_NAME(t.schema_id) + '.' + t.[Name] AS table_view
+	   ,CASE
+			WHEN t.[type] = 'U' THEN 'Table'
+			WHEN t.[type] = 'V' THEN 'View'
+		END AS [object_type]
+	   ,CASE
+			WHEN c.[type] = 'PK' THEN 'Primary key'
+			WHEN c.[type] = 'UQ' THEN 'Unique constraint'
+			WHEN i.[type] = 1 THEN 'Unique clustered index'
+			WHEN i.type = 2 THEN 'Unique index'
+		END AS constraint_type
+	   ,ISNULL(c.[Name], i.[Name]) AS constraint_name
+	   ,SUBSTRING(column_names, 1, LEN(column_names) - 1) AS [details]
+	FROM sys.objects t
+	LEFT OUTER JOIN sys.indexes i
+		ON t.object_id = i.object_id
+	LEFT OUTER JOIN sys.key_constraints c
+		ON i.object_id = c.parent_object_id
+		AND i.index_id = c.unique_index_id
+	CROSS APPLY (SELECT
+			col.[Name] + ', '
+		FROM sys.index_columns ic
+		INNER JOIN sys.columns col
+			ON ic.object_id = col.object_id
+			AND ic.column_id = col.column_id
+		WHERE ic.object_id = t.object_id
+		AND ic.index_id = i.index_id
+		ORDER BY col.column_id
+		FOR XML PATH ('')) D (column_names)
+	WHERE is_unique = 1
+	AND t.is_ms_shipped <> 1
+	UNION ALL
+	SELECT
+		SCHEMA_NAME(fk_tab.schema_id) + '.' + fk_tab.Name AS foreign_table
+	   ,'Table'
+	   ,'Foreign key'
+	   ,fk.Name AS fk_constraint_name
+	   ,SCHEMA_NAME(pk_tab.schema_id) + '.' + pk_tab.Name
+	FROM sys.foreign_keys fk
+	INNER JOIN sys.tables fk_tab
+		ON fk_tab.object_id = fk.parent_object_id
+	INNER JOIN sys.tables pk_tab
+		ON pk_tab.object_id = fk.referenced_object_id
+	INNER JOIN sys.foreign_key_columns fk_cols
+		ON fk_cols.constraint_object_id = fk.object_id
+	UNION ALL
+	SELECT
+		SCHEMA_NAME(t.schema_id) + '.' + t.[Name]
+	   ,'Table'
+	   ,'Check constraint'
+	   ,con.[Name] AS constraint_name
+	   ,con.[definition]
+	FROM sys.check_constraints con
+	LEFT OUTER JOIN sys.objects t
+		ON con.parent_object_id = t.object_id
+	LEFT OUTER JOIN sys.all_columns col
+		ON con.parent_column_id = col.column_id
+		AND con.parent_object_id = col.object_id
+	UNION ALL
+	SELECT
+		SCHEMA_NAME(t.schema_id) + '.' + t.[Name]
+	   ,'Table'
+	   ,'Default constraint'
+	   ,con.[Name]
+	   ,col.[Name] + ' = ' + con.[definition]
+	FROM sys.default_constraints con
+	LEFT OUTER JOIN sys.objects t
+		ON con.parent_object_id = t.object_id
+	LEFT OUTER JOIN sys.all_columns col
+		ON con.parent_column_id = col.column_id
+		AND con.parent_object_id = col.object_id) a
+ORDER BY table_view, constraint_type, constraint_name
