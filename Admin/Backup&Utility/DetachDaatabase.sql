b7583c0b10299b3d492ac48a6e5c7455ec14277f
= <=>
2022-04-04 07:01:37 +0430

    Add zero fill for installation SQL Server

--- Admin/Backup&Utility/DetachDaatabase.sql
+++ Admin/Backup&Utility/DetachDaatabase.sql
@@ -3,12 +3,27 @@ Before Detach database
 1. Check rout of files and save them
 2. Create one full backup
 3. CheckDB integrity DBCC CHECKDB
+4. Drop Connection
+5. Set singleuser
+6. Detach
 */
 
 --DBCC CHECKDB
 
-ALTER DATABASE TestDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+ALTER DATABASE TestDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE; --Detach with no wait
 GO
+
+USE master;
+ALTER DATABASE TestDB SET SINGLE_USER WITH ROLLBACK AFTER 2 Seconds;
+
+GO
+ALTER DATABASE TestDB SET MULTI_USER
+
+ALTER DATABASE TestDB 
+   SET OFFLINE;
+
+--ALTER DATABASE TestDB 
+--   SET ONLINE;
 
 USE master;
 
