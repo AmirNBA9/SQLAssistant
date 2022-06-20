e96cf945893685034686ab1a274e6c59e5481131
Amirnba9 <Amir.h.ghasemi@hotmail.com>
2022-06-20 14:58:40 +0430

    Edit jobs staus report

--- Admin/DBA/JobsStatus.sql
+++ Admin/DBA/JobsStatus.sql
@@ -2,19 +2,18 @@ USE msdb;
 
 GO
 /*Jobs_MinMaxAverageTime*/
-SELECT a.name AS 'JobName', a.description, --
-	   FORMAT(MAX (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "MaxDurationMinutes",
-       FORMAT(MIN (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "MinDurationMinutes",
-       FORMAT(AVG (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "AvgDurationMinutes"
-  FROM dbo.sysjobs AS a
-       LEFT JOIN dbo.sysjobhistory AS b ON b.job_id = a.job_id
-                                       AND b.step_id = 0
- WHERE a.enabled = 1
- GROUP BY a.name, a.description, b.step_name
- ORDER BY a.name;
+SELECT a.name AS 'JobName',
+       MAX(((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)) AS "MaxDurationMinutes",
+       MIN(((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)) AS "MinDurationMinutes",
+       AVG(((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)) AS "AvgDurationMinutes"
+FROM dbo.sysjobs AS a
+    LEFT JOIN dbo.sysjobhistory AS b ON b.job_id = a.job_id AND b.step_id = 0
+WHERE a.enabled = 1
+GROUP BY a.name
+ORDER BY a.name;
 
- /*Jobs_MinMaxAverageTime With steps*/
- SELECT a.name AS 'JobName', a.description, b.step_name, --
+/*Jobs_MinMaxAverageTime with steps*/
+SELECT a.name AS 'JobName', a.description, b.step_name,--
 	   FORMAT(MAX (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "MaxDurationMinutes",
        FORMAT(MIN (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "MinDurationMinutes",
        FORMAT(AVG (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "AvgDurationMinutes"
@@ -24,6 +23,7 @@ SELECT a.name AS 'JobName', a.description, --
  WHERE a.enabled = 1
  GROUP BY a.name, a.description, b.step_name
  ORDER BY a.name asc, step_name asc;
+
 
 /*FailedJobsReport*/
 SELECT SJ.name, SJS.last_outcome_message, SJS.last_run_date, SJS.last_run_duration
