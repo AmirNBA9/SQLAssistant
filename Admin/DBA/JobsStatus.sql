8decad8bad396bf8434ede5e872bbc9f88ebb99d
Amirnba9 <Amir.h.ghasemi@hotmail.com>
2022-06-20 16:28:33 +0430

    edit job status

--- Admin/DBA/JobsStatus.sql
+++ Admin/DBA/JobsStatus.sql
@@ -13,16 +13,28 @@ GROUP BY a.name
 ORDER BY a.name;
 
 /*Jobs_MinMaxAverageTime with steps*/
-SELECT a.name AS 'JobName', a.description, b.step_name,--
-	   FORMAT(MAX (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "MaxDurationMinutes",
-       FORMAT(MIN (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "MinDurationMinutes",
-       FORMAT(AVG (((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60)),'##,###') AS "AvgDurationMinutes"
+WITH t
+AS
+    (SELECT a.job_id, b.step_name, b.step_id, ((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100 + 31) / 60) AS run_duration,
+            CAST(DATEADD (MILLISECOND, ((b.run_duration / 10000 * 3600 + (b.run_duration / 100) % 100 * 60 + b.run_duration % 100)), '00:00:00') AS TIME(3)) AS run_duration_Format
+       FROM dbo.sysjobs AS a
+            LEFT JOIN dbo.sysjobhistory AS b ON b.job_id = a.job_id)
+SELECT a.name AS 'JobName', a.description, t.step_name, --
+       MAX (t.run_duration) AS "MaxDurationMinutes", --
+       MIN (t.run_duration) AS "MinDurationMinutes", --
+       AVG (t.run_duration) AS "AvgDurationMinutes", --
+       MIN (t.run_duration_Format) AS MinRunDuration, --
+	   MAX (t.run_duration_Format) AS MaxRunDuration
+
   FROM dbo.sysjobs AS a
-       LEFT JOIN dbo.sysjobhistory AS b ON b.job_id = a.job_id
-                                       AND b.step_id <> 0
+       LEFT JOIN t ON t.job_id = a.job_id
+                  AND t.step_id <> 0
  WHERE a.enabled = 1
- GROUP BY a.name, a.description, b.step_name
- ORDER BY a.name asc, step_name asc;
+ -- AND t.run_duration > 0 ==> Jobs less than 1 minutes is not important for check
+ GROUP BY a.name, a.description, t.step_name
+ ORDER BY a.name, "MinDurationMinutes" DESC;
+
+
 
 
 /*FailedJobsReport*/
