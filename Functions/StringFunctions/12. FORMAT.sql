ad740c58229fdfe0368d36a6e5da9e028e289c30
Amirhossein <49804441+AmirNBA9@users.noreply.github.com>
2022-12-09 23:15:25 +0330

    Update 12. FORMAT.sql

--- Functions/StringFunctions/12. FORMAT.sql
+++ Functions/StringFunctions/12. FORMAT.sql
@@ -28,3 +28,75 @@ SELECT FORMAT(cast('07:35' as time), N'hh:mm');   --> returns NULL
 
 SELECT FORMAT(cast('07:35' as time), N'hh\.mm');  --> returns 07.35  
 SELECT FORMAT(cast('07:35' as time), N'hh\:mm');  --> returns 07:35  
+
+/*Type of culture in format*/
+DECLARE @date datetimeoffset, @culture char(2); 
+SET @date = '2030-05-25 23:59:30.1234567 +03:30';
+SET @culture = 'fa';
+-- Persian (Shamsi)
+SELECT 
+  FORMAT(@date, 'd', @culture) AS 'd',
+  FORMAT(@date, 'D', @culture) AS 'D',
+  FORMAT(@date, 'f', @culture) AS 'f',
+  FORMAT(@date, 'F', @culture) AS 'F',
+  FORMAT(@date, 'g', @culture) AS 'g',
+  FORMAT(@date, 'G', @culture) AS 'G',
+  FORMAT(@date, 'm', @culture) AS 'm',
+  FORMAT(@date, 'M', @culture) AS 'M',
+  FORMAT(@date, 'o', @culture) AS 'o',
+  FORMAT(@date, 'O', @culture) AS 'O',
+  FORMAT(@date, 'r', @culture) AS 'r',
+  FORMAT(@date, 'R', @culture) AS 'R',
+  FORMAT(@date, 's', @culture) AS 's',
+  FORMAT(@date, 't', @culture) AS 't',
+  FORMAT(@date, 'T', @culture) AS 'T',
+  FORMAT(@date, 'u', @culture) AS 'u',
+  FORMAT(@date, 'U', @culture) AS 'U',
+  FORMAT(@date, 'y', @culture) AS 'y',
+  FORMAT(@date, 'Y', @culture) AS 'Y';
+
+-- Arabic (Hijri)
+SET @culture = 'ar';
+SELECT 
+  FORMAT(@date, 'd', @culture) AS 'd',
+  FORMAT(@date, 'D', @culture) AS 'D',
+  FORMAT(@date, 'f', @culture) AS 'f',
+  FORMAT(@date, 'F', @culture) AS 'F',
+  FORMAT(@date, 'g', @culture) AS 'g',
+  FORMAT(@date, 'G', @culture) AS 'G',
+  FORMAT(@date, 'm', @culture) AS 'm',
+  FORMAT(@date, 'M', @culture) AS 'M',
+  FORMAT(@date, 'o', @culture) AS 'o',
+  FORMAT(@date, 'O', @culture) AS 'O',
+  FORMAT(@date, 'r', @culture) AS 'r',
+  FORMAT(@date, 'R', @culture) AS 'R',
+  FORMAT(@date, 's', @culture) AS 's',
+  FORMAT(@date, 't', @culture) AS 't',
+  FORMAT(@date, 'T', @culture) AS 'T',
+  FORMAT(@date, 'u', @culture) AS 'u',
+  FORMAT(@date, 'U', @culture) AS 'U',
+  FORMAT(@date, 'y', @culture) AS 'y',
+  FORMAT(@date, 'Y', @culture) AS 'Y';
+
+-- English ()
+SET @culture = 'en-us';
+SELECT 
+  FORMAT(@date, 'd', @culture) AS 'd',
+  FORMAT(@date, 'D', @culture) AS 'D',
+  FORMAT(@date, 'f', @culture) AS 'f',
+  FORMAT(@date, 'F', @culture) AS 'F',
+  FORMAT(@date, 'g', @culture) AS 'g',
+  FORMAT(@date, 'G', @culture) AS 'G',
+  FORMAT(@date, 'm', @culture) AS 'm',
+  FORMAT(@date, 'M', @culture) AS 'M',
+  FORMAT(@date, 'o', @culture) AS 'o',
+  FORMAT(@date, 'O', @culture) AS 'O',
+  FORMAT(@date, 'r', @culture) AS 'r',
+  FORMAT(@date, 'R', @culture) AS 'R',
+  FORMAT(@date, 's', @culture) AS 's',
+  FORMAT(@date, 't', @culture) AS 't',
+  FORMAT(@date, 'T', @culture) AS 'T',
+  FORMAT(@date, 'u', @culture) AS 'u',
+  FORMAT(@date, 'U', @culture) AS 'U',
+  FORMAT(@date, 'y', @culture) AS 'y',
+  FORMAT(@date, 'Y', @culture) AS 'Y';
